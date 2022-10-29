fac04316dd689f08a3e45a0e9af3ad18bd45501a
Futhememe <kyoro_souza@hotmail.com>
2022-10-29 13:54:25 -0300

    ✨ new avatar component

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://avatars.githubusercontent.com/u/55204851?v=4',
+    alt: 'Gustavo S Souza',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
